e37bb12f72cfce6268d4a5bfc62df218edd499b0
jimpang <jimpang@tencent.com>
2024-03-22 17:52:13 +0800

    feat: 1. api适配venus修改; 2. 适配multi-lora

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -13,6 +13,7 @@ from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.llm_engine import LLMEngine
 from vllm.engine.ray_utils import initialize_ray_cluster, ray
 from vllm.logger import init_logger
+from vllm.lora.request import LoRARequest
 from vllm.outputs import RequestOutput
 from vllm.sampling_params import SamplingParams
 
@@ -307,6 +308,9 @@ class AsyncLLMEngine:
         self.log_requests = log_requests
         self.max_log_len = max_log_len
         self.engine = self._init_engine(*args, **kwargs)
+        # jimpang: for lora
+        self.lora_names_map = {}
+        self.last_lora_id = 1
 
         self.background_loop = None
         # We need to keep a reference to unshielded
@@ -610,6 +614,15 @@ class AsyncLLMEngine:
         """
         # Preprocess the request.
         arrival_time = time.time()
+
+        # jimpang: process lora id
+        if lora_request:
+            if lora_request.lora_name in self.lora_names_map:
+                lora_request.lora_int_id = self.lora_names_map[lora_request.lora_name]
+            else:
+                self.last_lora_id = self.last_lora_id + 1
+                lora_request.lora_int_id = self.last_lora_id
+                self.lora_names_map[lora_request.lora_name] = lora_request.lora_int_id
 
         try:
             stream = await self.add_request(
