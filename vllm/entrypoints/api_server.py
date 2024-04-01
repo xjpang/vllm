49e54750340abdedcd9f49050bc0aa8ee56deb09
jimpang <jimpang@tencent.com>
2024-04-01 20:50:35 +0800

    feat: 支持返回logprobs

--- vllm/entrypoints/api_server.py
+++ vllm/entrypoints/api_server.py
@@ -8,6 +8,7 @@ change `vllm/entrypoints/openai/api_server.py` instead.
 
 import json
 import ssl
+from dataclasses import asdict
 from typing import AsyncGenerator
 
 import uvicorn
@@ -77,7 +78,8 @@ async def generate(request: Request) -> Response:
                 output.text for output in request_output.outputs
             ]
             output_tokens = [output.token_ids for output in request_output.outputs]
-            logprobs = [output.logprobs for output in request_output.outputs]
+            logprobs = [[{k: asdict(v) for k, v in logprobs.items()} for logprobs in
+                         output.logprobs] if output.logprobs is not None else None for output in request_output.outputs]
             ret = {"text": text_outputs, "output_token_ids": output_tokens, "logprobs": logprobs}
             yield (json.dumps(ret) + "\0").encode("utf-8")
 
@@ -96,7 +98,8 @@ async def generate(request: Request) -> Response:
     assert final_output is not None
     text_outputs = [output.text for output in final_output.outputs]
     output_tokens = [output.token_ids for output in final_output.outputs]
-    logprobs = [output.logprobs for output in final_output.outputs]
+    logprobs = [[{k: asdict(v) for k, v in logprobs.items()} for logprobs in
+                 output.logprobs] if output.logprobs is not None else None for output in final_output.outputs]
     ret = {"text": text_outputs, "output_token_ids": output_tokens, "logprobs": logprobs}
     return JSONResponse(ret)
 
