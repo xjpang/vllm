f65eb957ee5f1e0283994bbb91fd185addd08cf4
jimpang <jimpang@tencent.com>
2025-11-11 19:49:19 +0800

    feat: venus adapt

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -1366,15 +1366,19 @@ async def run_server_worker(
 
 
 if __name__ == "__main__":
-    # NOTE(simon):
-    # This section should be in sync with vllm/entrypoints/cli/main.py for CLI
-    # entrypoints.
-    cli_env_setup()
-    parser = FlexibleArgumentParser(
-        description="vLLM OpenAI-Compatible RESTful API server."
-    )
-    parser = make_arg_parser(parser)
-    args = parser.parse_args()
-    validate_parsed_serve_args(args)
+    try:
+        # NOTE(simon):
+        # This section should be in sync with vllm/entrypoints/cli/main.py for CLI
+        # entrypoints.
+        cli_env_setup()
+        parser = FlexibleArgumentParser(
+            description="vLLM OpenAI-Compatible RESTful API server."
+        )
+        parser = make_arg_parser(parser)
+        args = parser.parse_args()
+        validate_parsed_serve_args(args)
 
-    uvloop.run(run_server(args))
+        uvloop.run(run_server(args))
+    except Exception as e:
+        logger.error(str(e))
+        raise
